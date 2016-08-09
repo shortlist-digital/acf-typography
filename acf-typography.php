5c6d95ca8ef6c1d96da476bfa1288f2f3d052337
Jon Sherrard <jon.sherrard@shortlist.com>
2016-08-09 11:15:10 -0400

    Applied fixes from StyleCI

--- acf-typography.php
+++ acf-typography.php
@@ -1,12 +1,12 @@
-<?php namespace CustomFieldTypography;
+<?php
 
-require __DIR__ . '/vendor/autoload.php';
+namespace CustomFieldTypography;
 
-use Michelf\SmartyPants;
-use add_filter;
+require __DIR__.'/vendor/autoload.php';
+
 use add_action;
-use get_field;
-
+use add_filter;
+use Michelf\SmartyPants;
 
 /*
 Plugin Name: ACF Typography
@@ -18,38 +18,45 @@ Author URI: http://twitter.com/jshez
 License: GPL2
 */
 
-class CustomFieldTypography {
-
-  function __construct() {
-    add_action('after_setup_theme', array($this, 'setup_filters'));
-  }
-
-  function setup_filters() {
-    add_filter('acf/update_value/type=text', array($this, 'format'), 13, 3);
-    add_filter('acf/update_value/type=wysiwyg', array($this, 'format'), 13, 3);
-    add_filter('acf/update_value/type=strict_wysiwyg', array($this, 'format'), 13, 3);
-    add_filter('wp_insert_post_data', array($this, 'format_title'), 13, 2);
-  }
-
-  function format_title($data, $post_arr) {
-    $data['post_title'] = $this->do_format($data['post_title']);
-    return $data;
-  }
-
-  function do_format($value) {
-    $value = stripcslashes($value);
-    $value = SmartyPants::defaultTransform($value);
-    $value = html_entity_decode($value);
-    return $value;
-  }
-
-  function format($value, $post_id = null , $field= null) {
-    if (!empty($value)) {
-      $value = $this->do_format($value);
-      return $value;
+class CustomFieldTypography
+{
+    public function __construct()
+    {
+        add_action('after_setup_theme', [$this, 'setup_filters']);
     }
-  }
 
+    public function setup_filters()
+    {
+        add_filter('acf/update_value/type=text', [$this, 'format'], 13, 3);
+        add_filter('acf/update_value/type=wysiwyg', [$this, 'format'], 13, 3);
+        add_filter('acf/update_value/type=strict_wysiwyg', [$this, 'format'], 13, 3);
+        add_filter('wp_insert_post_data', [$this, 'format_title'], 13, 2);
+    }
+
+    public function format_title($data, $post_arr)
+    {
+        $data['post_title'] = $this->do_format($data['post_title']);
+
+        return $data;
+    }
+
+    public function do_format($value)
+    {
+        $value = stripcslashes($value);
+        $value = SmartyPants::defaultTransform($value);
+        $value = html_entity_decode($value);
+
+        return $value;
+    }
+
+    public function format($value, $post_id = null, $field = null)
+    {
+        if (!empty($value)) {
+            $value = $this->do_format($value);
+
+            return $value;
+        }
+    }
 }
 
 new CustomFieldTypography();
